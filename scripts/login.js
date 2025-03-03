b85c298d7251b5431b93388121f7aa04aa10b50d
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-03-03 18:42:10 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -14,7 +14,7 @@ async function login(username, password) {
         password
     }
 
-    const response = await fetch("http://localhost:3000/api/auth/", {
+    const response = await fetch("https://lowly-cool-pony.glitch.me/api/auth/", {
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
