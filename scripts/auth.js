5c3a4ab6d9f18e362987421c95c46621db14bd52
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-03-08 23:41:08 -0500

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,7 +7,7 @@ class Auth {
 
     validateAuth(auth) {
         if(auth != 1) {
-            window.location.replace("/login.html")
+            window.location.replace("login.html")
         }
         else {
             document.querySelector("body").style.display = "block"
